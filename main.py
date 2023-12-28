8ec145a582db5574b2cf2f5d163dd2b51af5ccd8
gand0rf <gand0rf@protonmail.com>
2023-12-28 06:54:24 -0500

    Inital upload

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup as BS
+import os
+
+def get_fonts():
+    url = "https://www.nerdfonts.com/font-downloads"
+    font_list = []
+    result = requests.get(url).text
+    soup = BS(result, "html.parser")
+    a_list = soup.find_all('a', href=True)
+
+    for a in a_list:
+        if "github" in str(a):
+            zip = a.get("href")
+            if ".zip" in str(zip) and str(zip) not in font_list:
+                font_list.append(str(zip))
+
+    return font_list
+
+def dload_fonts(font_list):
+    home = os.path.expanduser('~')
+    if not os.path.exists(f'{home}/Downloads/fonts'):
+        os.makedirs(f'{home}/Downloads/fonts')
+    
+    os.chdir(f'{home}/Downloads/fonts')
+
+    for font in range(0,len(font_list)):
+        print(f'Getting {font} out of {len(font_list)}..', end='\r')
+        os.system(f'wget {font_list[font]} >/dev/null 2>&1')
+    
+    return 0
+
+def unzip_fonts():
+    home = os.path.expanduser('~')
+    if not os.path.exists(f'{home}/.local/share/fonts'):
+        ps.makedirs(f'{home}/.local/share/fonts')
+    os.system(f"unzip -o '*.zip' -d {home}/.local/share/fonts >/dev/null 2>&1")
+
+def zip_remove():
+    os.system('rm *.zip')
+
+if __name__ == "__main__":
+    print('Getting font urls...')
+    font_list = get_fonts()
+    print('Font list made')
+    print('Downloading font archives...')
+    dload_fonts(font_list)
+    print('Downloading completed')
+    print('Unzipping fonts...')
+    unzip_fonts()
+    print('Unzipping completed')
+    remove = input('Remove font zip archives? [y/n] ')
+    if remove == 'y':
+        zip_remove()
+    print('Installation of Nerd Fonts complete. Enjoy!!')
+            
---
+++ readme.txt
@@ -0,0 +1,13 @@
+NF_DLOAD
+
+A script for automating the download and installation of nerd fonts.
+
+If you want the full set of nerd fonts, but are having trouble cloning the repo or just
+don't want to manualy download each font, this script can help you.
+
+As of right now it is setup to work with Linux systems. I do plan on adding Windows and Mac support.
+
+Right now is gets the download links from nerd fonts website, but I do
+plan on pointing it to the github repo in the future to help stay current incase
+they do not update the links on the website.
+
